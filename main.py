f5b488d1066e73163cd72c9bf628509525d94b9e
Yusuf Putra Bintang Satria <92704969+YusufPutraBintangSatria@users.noreply.github.com>
2022-01-11 16:29:54 -1200

    "Menambahkan file-file program python"

---
+++ main.py
@@ -0,0 +1,45 @@
+
+
+
+def menu():
+    print('=====================================')
+    print('Input Data Nilai Mahasiswa'.center(40))
+    print('=====================================')
+    print('|    1. Tambah Data                 |')
+    print('|    2. Cari Data                   |')
+    print('|    3. Tampilkan Data Mahasiswa    |')
+    print('|    4. Ubah Data Mahasiswa         |')
+    print('|    5. Hapus Data Mahasiswa        |')
+    print('|    6. Selesai                     |')
+    print('=====================================')
+
+
+while True:
+    menu()
+    choose = input('Pilih Menu  : ')
+
+    if choose == '1':
+       
+        input('Untuk kembali ke menu utama, tekan enter')
+
+    elif choose == '2':
+       
+        input('Untuk kembali ke menu utama, tekan enter')
+
+    elif choose == '3':
+       
+        input('Untuk kembali ke menu utama, tekan enter')
+
+    elif choose == '4':
+        
+        input('Untuk kembali ke menu utama, tekan enter')
+
+    elif choose == '5':
+       
+        input('Untuk kembali ke menu utama, tekan enter')
+
+    elif choose == '6':
+        exit()
+
+    else:
+        print("Menu yang anda maksud tidak tersedia, Silahkan pilih menu yang tersedia")
